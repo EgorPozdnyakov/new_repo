69f2f4c9d771b6b0eadb6d04b3018b6a65005f65
Egor Pozdnyakov <egor_008@list.ru>
2023-02-13 18:41:45 +0300

    1

---
+++ main.py
@@ -0,0 +1,9 @@
+from roman import *
+
+roman1 = [str(vvodR) for vvodR in input('Введите римское число:').split()]
+for i in roman1:
+    print(i, '->' ,roman_to_int(i))
+
+arabic= [int(vvodA) for vvodA in input('Введите арабское число:').split()]
+for i in arabic:
+    print(i, '->',int_to_roman(i))
